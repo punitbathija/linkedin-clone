f4b8ec53e898a3ca215ddda21628172c362d49f6
Punit Bathija <punit644@gmail.com>
2022-12-16 17:46:23 +0530

    Working on header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import logo from "../assets/logo.svg";
+import HeaderOptions from "./HeaderOptions";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-left">
+        <img src={logo} alt="" />
+        <div className="header_search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header-right">
+        <HeaderOptions Icon={HomeIcon} title="Home" />
+        <HeaderOptions Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOptions Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOptions Icon={ChatIcon} title="Messaging" />
+        <HeaderOptions Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOptions avatar="" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
