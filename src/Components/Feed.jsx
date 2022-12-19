6f691d45590fd1097191d8f15fa15b2368079480
Punit Bathija <punit644@gmail.com>
2022-12-19 14:09:24 +0530

    Error

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./feed.css";
 import CreateIcon from "@mui/icons-material/Create";
 import InputOption from "./Input_Option";
@@ -6,16 +6,67 @@ import ImageIcon from "@mui/icons-material/Image";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
+import Post from "./Post";
+import { initializeApp } from "firebase/app";
+
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  onSnapshot,
+  doc,
+  addDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzVKJf4uOyQxyWYaLD39pkd0_Up2u2gnY",
+  authDomain: "linked-in-clone-464d9.firebaseapp.com",
+  projectId: "linked-in-clone-464d9",
+  storageBucket: "linked-in-clone-464d9.appspot.com",
+  messagingSenderId: "854248874047",
+  appId: "1:854248874047:web:a3295eef90c80aec7b988a",
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+  const [input, setInput] = useState("");
+
+  const colRef = collection(db, "posts");
+
+  const dbData = onSnapshot(colRef, (snapshot) => {
+    setPosts(
+      snapshot.docs.map((doc) => {
+        data: doc.data();
+      })
+    );
+  });
+
+  const sendPost = (e) => {
+    e.preventDefault();
+    addDoc(colRef, {
+      name: "Punit",
+      description: "Engineer",
+      message: input,
+      photoUrl: "",
+    });
+  };
+
   return (
     <div className="feed">
       <div className="feed_inputContainer">
         <div className="feed_input">
           <CreateIcon />
           <form>
-            <input type="text" />
-            <button type="submit">Send</button>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
           </form>
         </div>
         <div className="feed_inputOptions">
@@ -29,6 +80,15 @@ const Feed = () => {
           />
         </div>
       </div>
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
+      ))}
     </div>
   );
 };
